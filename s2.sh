a0d751c77eb31c12d8fc59bcb4d4f8f596eaee7d
Jonathan Tito <jotitoon@uide.edu.ec>
2026-05-19 01:43:49 +0000

    wip: basic commands

--- s2.sh
+++ s2.sh
@@ -16,3 +16,18 @@ cd
 # ruta relativa: no tiene la ruta completa
 # lista archivos y directorios en el directorio actual
 ls
+#ls: lista archivos y directorios en el directorio actual 
+# y la opcion -a es all de muestrame todo 
+#(incluyendo archivos y directorios ocultos) 
+ls -a
+# cambio de directorio al directorio raíz de linux
+cd /
+# cambio de directorio al directorio de binarios de linux
+#ruta absoluta
+cd /bin 
+#ruta relativa
+cd bin
+# 
+sudo cp /workspaces/intro-unix-may-2025-online/hola_binario /bin/hola_binario
+# make direcory
+mkdir nuevo_directorio
