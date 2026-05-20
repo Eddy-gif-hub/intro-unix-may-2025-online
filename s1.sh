919ba44ed71e20e397018e120c5de48624f9edb2
Eddy Javier Madrid Rosero <edmadridro@uide.edu.ec>
2026-05-20 04:05:14 +0000

    wip: first commands and C code

--- s1.sh
+++ s1.sh
@@ -1,4 +1,4 @@
-# Me muestra la nube sobre la que corre esa VM
+# Me muestra la nube sobre la que corre es VM(Muestra ka version del kernel)
 uname -r
 # Me muestra toda la información de mi sistema operativo y el hardware asociado
 uname -a
---
+++ s2.sh
@@ -0,0 +1,13 @@
+#
+man gcc
+#
+gcc --help
+# 
+gcc primer.c -o hola_binario
+#
+file hola_binario
+#
+file primer.c
+#
+
+
