8841e0aa9b18bb2e9ed0993ac479ff1ce3a71a29
Eddy Javier Madrid Rosero <edmadridro@uide.edu.ec>
2026-05-22 01:54:07 +0000

    wip:final commit s3

---
+++ s3.sh
@@ -0,0 +1,57 @@
+#pwd 
+pwd
+#ls
+ls
+#mkdir(crea directorios)
+mkdir
+#cd(change directory) cd/workspaces/intro-unix-may-2025-online/padre
+cd
+#mkdir
+mkdir
+#ls --all (muestra todos los archivos ocultos tambien)
+ls --all
+#cd ..(Es el moverse un directorio antes)
+cd..
+# ls -a ( Es la forma abreviada )
+ls -a  
+# cd . (direcorio actual)
+cd .
+# man ls
+man ls
+# touch (crea archivos pero debes poner el .txt o especificar el tipo del archivo)
+touch 
+# cd ruta absoluta 
+cd/workspaces/intro-unix-may-2025-online/padre
+# cd ruta relativa
+cd padre
+# cd /
+
+# cd ls
+
+# cd workspaces/
+
+# es case sensitive por las mayusculas y minusculas pero tengo entendido que solo se usa misnusculas
+#VARIABLE DE ENTORNO
+# echo $HOME(echo imprime informacion en la terminal)
+echo HOME
+#printf ( es un modo de impresion pero incompleto)
+printf
+# cd (sin ningun argumento te envia al home al directorio principal o HOME)
+cd
+# cd ~ (te lleva igual al directorio raiz o HOME)
+cd ~
+# rmdir (elimina solo directorios)
+
+# rm ( elimina archivos a lado el nombre del archivo)
+
+# cp (copiar)
+
+# mv (renombra el archivo)
+
+# cp -r padre destino/
+
+#mv destino/padre/HIJO .
+
+#ls dev/(dispositivos)
+
+
