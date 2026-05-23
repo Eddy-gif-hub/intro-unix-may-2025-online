37c1b6ecb497c56506498414a471bcf8c2676b00
Eddy Javier Madrid Rosero <edmadridro@uide.edu.ec>
2026-05-23 20:10:45 +0000

    wip: continuation of the class

---
+++ confirmacion_deber.sh
@@ -0,0 +1,7 @@
+# para trasnformar el codigo en lengiuejentendlble a codigo máquina
+gcc chanchito.c -o chanchito_feliz
+
+# Con esto ya esto listo para ejectuar el comando
+
+# Aqui ya logre ver el resultado de la compilacion lo que pide el ejercicio 
+./chanchito_feliz
--- s2.sh
+++ s2.sh
@@ -1,13 +1,18 @@
-#
+#Nos da el manual de un comando (informacion completa)
 man gcc
-#
+#Nos da informacion necesaria del comando (solo lo que necesitas saber)
 gcc --help
-# 
-gcc primer.c -o hola_binario
-#
+# Es la manera como se traduce el codigo en codigo binario(lenguaje maquina)
+gcc primer.c -o hola_binario # El nombre se pone sino te pone un por defecto(a.out)
+# El formato ejecutable de linux
 file hola_binario
-#
+# Es un formato legible para el humano
 file primer.c
-#
+#Ruta absoluta (Es la ruta completa)
+/workspaces/intro-unix-may-2025-online/chanchito.c
+#Ruta relativa (Es la ruta específica)
+/chanchito_feliz
+#ls (Me da la infornmación del directorio donde se encuentra)
+ls
 
 
