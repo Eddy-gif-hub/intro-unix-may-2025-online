c0abb96bc58b2bda5752c92b8ae079eb89d79f2e
Eddy Javier Madrid Rosero <edmadridro@uide.edu.ec>
2026-05-29 01:26:06 +0000

    wip: The class week four

---
+++ s4.sh
@@ -0,0 +1,24 @@
+# touch (crea un archivo)
+touch
+# ls -la ()
+ls -la
+# chmod
+
+# sudo su
+
+# id
+
+# exit
+
+# chmod 457
+
+# umask
+
+# cd ~ Es el home de la comcd
+
+# touch mi_archivo_3
+
+# chmod 644
+
+
+
