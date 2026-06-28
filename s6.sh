09d200d36b929da1b3251b31aa6617bb31d60e4f
Eddy Javier Madrid Rosero <edmadridro@uide.edu.ec>
2026-06-28 03:07:22 +0000

    wip: ping with arguments

---
+++ s6.sh
@@ -0,0 +1,17 @@
+# cd /bin (change directory del directorio binario)
+cd /bin
+# ls | grep "bash" (nos encontro las coincidencias de la palabra bash con la tuberia | pyaland une comandos)
+ls | grep "bash"
+# bash /workspaces/intro-unix-may-2025-online/ping_with_arguments.sh (ejecutamos el script)
+ bash /workspaces/intro-unix-may-2025-online/ping_with_arguments.sh
+# bash /workspaces/intro-unix-may-2025-online/ping_with_arguments.sh 127.0.0.1 ( la variable uno)
+bash /workspaces/intro-unix-may-2025-online/ping_with_arguments.sh 127.0.0.1
+# sudo apt install iputils-ping (Instala el comando ping)
+sudo apt install iputils-ping
+# ctrl + c (parar un proceso en marcha)
+ctrl + c
+# env ( lista de todas las varibales de entorno del sistema operativo)
+env
+
+
+
