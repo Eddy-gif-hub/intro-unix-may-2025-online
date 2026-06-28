bb8189d99bde8e2fdf747f1216e703d72067a74a
Eddy Javier Madrid Rosero <edmadridro@uide.edu.ec>
2026-06-28 02:20:00 +0000

    wip: actualization of my commit

--- s5.sh
+++ s5.sh
@@ -1,11 +1,13 @@
+# nano (Es un editor en el cual creamos la funcion )
 nano
-
+# cat ( me muestra lo que esta dentro de una carpeta)
 cat
-# suma.py
+# suma.py ( Es el archivo donde se creo la funcion)
 suma.py
-# calculadora.pt
-# sudo apt update()
+# calculadora.py (es el ejecutable)
+calculadora.py
+# sudo apt update(para actualizar lista de paquetes disponibles)
 sudo apt update
-
+#sudo apt upgrade(para actualizar los programas instalados)
 sudo apt upgrade
 
