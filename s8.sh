804e727ee11f2fd51f61c05c2a7b022399af9e4b
Eddy Javier Madrid Rosero <edmadridro@uide.edu.ec>
2026-06-28 16:55:56 +0000

    wip: Final exercise with editor vi

--- s8.sh
+++ s8.sh
@@ -1,1 +1,9 @@
-# Filtrar datos
+#!/bin/bash
+SIGNAL_TO_STOP_FILE="stoop_look"
+
+while [[ ! -f "${SIGNAL_TO_STOP_FILE}" ]];do
+	echo "El aarchivo ${SIGNAL_TO_STOP_LINE} todavia no existe"
+	echo "Comprobando de nuevo en 2 segundos"
+	sleep 2
+done
+echo "El archivo fue encontrado! Saliendo"
