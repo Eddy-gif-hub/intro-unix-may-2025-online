09175f2dbc1c81f3261c77c369f9ec56674e2a13
Eddy Javier Madrid Rosero <edmadridro@uide.edu.ec>
2026-06-28 02:08:24 +0000

    wip: Actualization of mi commit.

--- s4.sh
+++ s4.sh
@@ -1,26 +1,26 @@
 # touch (crea un archivo)
 touch
-# ls -la ()
+# ls -la (lista o muestra archivos ocultos)
 ls -la
-# chmod
-
-# sudo su
-
-# id
-
-# exit
-
-# chmod 457
-
-# umask
-
-# cd ~ Es el home de la comcd
-
-# touch mi_archivo_3
-
-# chmod 644
-
+# chmod ( sirve para cambiar los permisos de un archivo)
+chmod
+# sudo su ( me permite convertirme en el super usuario)
+sudo su
+# id (sirve para mostrar la identidad del usuario)
+id 
+# exit (Es la salida de cualquier comadno se este ejecutando)
+exit
+# chmod 457(Es el modo numérico de permisos de Linux)
+chmod 457
+# umask (se usa para definir los permisos predeterminados que tendran los archivos y directorios)
+umask 
+# cd ~ ( cambia a al carpeta home)
+cd~
+# touch mi_archivo_3(Cree un nuevo archivo con touch)
+touch 
+# chmod 644 (Es el permiso por defecto)
+chmod 644
 # stat mi_archivo_3 Muesgta la informacion detallada y metadatos de un archivo o sistema de archivos
-
+stat
 
 
